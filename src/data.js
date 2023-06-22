bc923ad001920294124d73ecc5004551713a79e6
gaechkahub <keozon@gmail.com>
2023-06-22 14:52:11 +0200

    Added title, favicon, 10 questions, made 4 answers for all questions and wrote a little CSS

--- src/data.js
+++ src/data.js
@@ -19,6 +19,7 @@ export const quizData = {
         a: 'constant, let, variable',
         b: 'var, const, let, function',
         c: 'var, let, const',
+        d: 'let, const'
       },
       correct: 'c',
       selected: null,
@@ -39,7 +40,7 @@ export const quizData = {
         a: 'changes the type of a primitive value',
         b: 'returns a string describing the type of a value',
         c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        d: 'can tell the difference between arrays and objects'
       },
       correct: 'b',
       selected: null,
@@ -55,6 +56,93 @@ export const quizData = {
         },
       ],
     },
-    // Add more questions here
+    {
+      text: 'Which of the following functions doesn\'t take a callback function as one of its parameters?',
+      answers: {
+        a: '.addEventListener()',
+        b: '.splice()',
+        c: '.reduce()',
+        d: '.filter()'
+      },
+      correct: 'b',
+      selected: null
+    },
+    {
+      text: 'What is the value of: ”41” + 1',
+      answers: {
+        a: '42',
+        b: '411',
+        c: '“42”',
+        d: '“411”'
+      },
+      correct: 'd',
+      selected: null
+    },
+    {
+      text: 'What would this piece of code display? let x = 5; console.log(x != 8);',
+      answers: {
+        a: 'false',
+        b: 'true',
+        c: 'x!=8',
+        d: '8'
+      },
+      correct: 'b',
+      selected: null
+    },
+    {
+      text: 'Which type of language is Javascript?',
+      answers: {
+        a: 'Programming',
+        b: 'Markup',
+        c: 'Scripting',
+        d: 'None of the above'
+      },
+      correct: 'c',
+      selected: null
+    },
+    {
+      text: 'Purpose of designing the Javascript',
+      answers: {
+        a: 'To Perform Server Side Scripting Operations',
+        b: 'To Style HTML Pages',
+        c: 'To add interactivity to HTML Pages',
+        d: 'All of the above'
+      },
+      correct: 'c',
+      selected: null
+    },
+    {
+      text: 'Which of the following function of Array object removes the last element from an array and returns that element?',
+      answers: {
+        a: 'push()',
+        b: 'pop()',
+        c: 'delete()',
+        d: 'link()'
+      },
+      correct: 'b',
+      selected: null
+    },
+    {
+      text: 'Which is not valid data type in Javascript?',
+      answers: {
+        a: 'Undefined',
+        b: 'float',
+        c: 'Boolean',
+        d: 'Number'
+      },
+      correct: 'b',
+      selected: null
+    },
+    {
+      text: 'The _______ method of an Array object adds and/or removes elements from an array.',
+      answers: {
+        a: 'Reverse',
+        b: 'Slice',
+        c: 'Shift',
+        d: 'Splice'
+      },
+      correct: 'c',
+      selected: null
+    },
   ],
 };
